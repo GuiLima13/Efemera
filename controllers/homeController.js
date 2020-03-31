549355ed9a1a7de161c0926400b513f01d594a04
Guilherme Lima <guilherme.lima13@yahoo.com.br>
2020-03-31 17:31:55 -0300

    Criando contato e painel admin

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const fs = require('fs');
 const homeController = {
     index: (req,res) => {
         let servicos = [
@@ -14,9 +16,66 @@ const homeController = {
         ]
         res.render('index',{title: 'Home' , listaServicos: servicos, carousel: slider});
     },
+
+    contato: (req,res)=>{
+        let {nomeC,emailC,mensagemC} = req.body;
+        let contato ={nomeC,emailC,mensagemC};
+
+
+        let fileContato = path.join('db','contatos.json');
+        let listaContato =[];
+
+        if(fs.existsSync(fileContato)){
+            //Trazer informações do arquivo
+            listaContato = fs.readFileSync(fileContato,{encoding:'utf-8'});
+            listaContato = JSON.parse(listaContato);
+ 
+        }
+               
+        //salvar informações no arquivo
+        listaContato.push(contato);
+        listaContato = JSON.stringify(listaContato);
+
+        fs.writeFileSync(fileContato, listaContato);
+
+        res.render('contato',{nomeC,title: 'Home'});
+    },
     news: (req,res)=>{
-        let {nomeNews, emailNews} = req.body;
+        let {nomeNews, email} = req.body;
+        let info ={nomeNews,email};
+
+
+        let fileNews = path.join('db','news.json');
+        let listaNews =[];
+
+        if(fs.existsSync(fileNews)){
+            //Trazer informações do arquivo
+            listaNews = fs.readFileSync(fileNews,{encoding:'utf-8'});
+            listaNews = JSON.parse(listaNews);
+ 
+        }
+               
+        //salvar informações no arquivo
+        listaNews.push(info);
+        listaNews = JSON.stringify(listaNews);
+
+        fs.writeFileSync(fileNews, listaNews);
+
         res.render('news',{nomeNews,title: 'Home'});
+    },
+    admin: (req,res)=>{
+
+        let fileNews = path.join('db','news.json');
+
+        listaNews = fs.readFileSync(fileNews,{encoding:'utf-8'});
+        listaNews = JSON.parse(listaNews);
+
+        let fileContatos = path.join('db','contatos.json');
+
+        listaContatos = fs.readFileSync(fileContatos,{encoding:'utf-8'});
+        listaContatos = JSON.parse(listaContatos);
+
+        res.render('admin',{listaNews,listaContatos, title:"Painel De Controle"});
     }
     
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const homeController = require('../controllers/homeController')
 /* GET home page. */
 router.get('/', homeController.index);
 router.post('/newsletter', homeController.news);
+router.post('/contato', homeController.contato);
+router.get('/painelcontrole', homeController.admin);
 
 module.exports = router;
